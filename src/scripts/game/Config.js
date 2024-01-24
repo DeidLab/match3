795c18a715e90e1c6057614864bdad3ad1c7e9fa
Davide Ferrero <d.ferrero@me.com>
2024-01-24 15:58:42 +0100

    first commit

---
+++ src/scripts/game/Config.js
@@ -0,0 +1,17 @@
+import { Game } from "./Game";
+import { Tools } from "../system/Tools";
+
+export const Config = {
+    loader: Tools.massiveRequire(require["context"]('./../../sprites/', true, /\.(mp3|png|jpe?g)$/)),
+    startScene: Game,
+    tilesColors: ['0', '1', '2', '3', '4', '5'],
+    board: {
+        rows: 8,
+        cols: 8
+    },
+    combinationRules: [[
+        {col: 1, row: 0}, {col: 2, row: 0},
+    ], [
+        {col: 0, row: 1}, {col: 0, row: 2},
+    ]]
+};
